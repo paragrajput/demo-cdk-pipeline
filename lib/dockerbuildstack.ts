2a9d64eab403ef16e560179a0c80f90f6180cdd3
Parag <paragrajput52@gmail.com>
2024-02-26 11:44:15 +0530

    Add DockerFIle

--- lib/dockerbuildstack.ts
+++ lib/dockerbuildstack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import { Construct } from "constructs";
+import path = require("path");
 
 
 interface dockerbuildstackProps extends StackProps{
@@ -9,6 +11,10 @@ interface dockerbuildstackProps extends StackProps{
 export class dockerbuildstack extends Stack {
     constructor(scope: Construct, id: string, props: dockerbuildstackProps) {
         super(scope, id, props);
-        
+       
+        const asset = new DockerImageAsset(this, 'BuildDockerImage', {
+            directory: path.join(__dirname, '../docker'),
+            assetName: "Nginx_Image",
+          });
     }
 }
